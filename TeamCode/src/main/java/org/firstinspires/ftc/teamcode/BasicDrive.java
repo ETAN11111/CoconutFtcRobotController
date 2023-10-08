2d771ceccf981eb87db742c6fc1eaa7d4ffc41bc
ETAN1111 <ebxu14@gmail.com>
2023-10-07 17:26:06 -0700

    hardwaremap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous
+public class BasicDrive extends LinearOpMode{
+
+
+    private DcMotor         backleft   = null;
+    private DcMotor         frontleft  = null;
+    private DcMotor         backright   = null;
+    private DcMotor         frontright  = null;
+
+    backleft  = hardwareMap.get(DcMotor.class, "back_left");
+    frontleft = hardwareMap.get(DcMotor.class, "front_left");
+    backright  = hardwareMap.get(DcMotor.class, "back_right");
+    frontright = hardwareMap.get(DcMotor.class, "front_right");
+
+
+
+}
